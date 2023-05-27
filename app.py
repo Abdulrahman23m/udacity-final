27151818286396b28a636a30c0e5fdd8bd5ab15c
Abdulrahman T <abdulrahmant@Abdulrahmans-MacBook-Pro.local>
2023-05-28 00:20:51 +0300

    added url

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from models import setup_db, Actors, Movies
+
+
+def create_app(test_config=None):
+  # create and configure the app
+  app = Flask(__name__)
+  setup_db(app)
+  CORS(app)
+  @app.route('/', methods=["GET"])
+  def get_init():
+     return jsonify({
+        'test_url':os.environ['EXCITED']
+     })
+  
+  return app
+
+app = create_app()
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
