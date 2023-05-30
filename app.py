b6ab64f2db569c672e2deab4da8a9dcb06fdf364
Abdulrahman T <abdulrahmant@Abdulrahmans-MacBook-Pro.local>
2023-05-30 20:41:45 +0300

    listen

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from models import setup_db, Actors, Movies
+from auth import AuthError, requires_auth
 
 
 def create_app(test_config=None):
@@ -16,11 +17,40 @@ def create_app(test_config=None):
         'test_url':os.environ['EXCITED']
      })
   
+  #added for CRUD requests
+  @app.after_request
+  def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Headers',
+                             'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
+  #For each requests we must check auth for the user by @requires_auth
+
+  @app.route('/actors', methods=['GET'])
+  @requires_auth(permission='get:actors')
+  def get_actors(payload):
+        try:
+            actors = Actors.query.order_by(Actors.id).all()
+            total_actors = len(actors)
+            return jsonify({
+                'success': True,
+                'actors': actors,
+                'total-actors': total_actors
+            })
+        except Exception:
+            abort(422)
+  
+
+  
+
   return app
+
+   
 
 app = create_app()
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080,debug=True)
