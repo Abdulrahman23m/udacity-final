91638885868674c7341b587ec8161e9bd2b55353
Abdulrahman T <abdulrahmant@Abdulrahmans-MacBook-Pro.local>
2023-06-06 12:04:09 +0300

    issue

--- auth.py
+++ auth.py
@@ -31,6 +31,7 @@ class AuthError(Exception):
 
 def get_token_auth_header():
     auth = request.headers.get('Authorization', None)
+    print(auth)
     if not auth:
         raise AuthError({
             'code': 'authorization_header_missing',
